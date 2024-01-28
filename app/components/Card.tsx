cf456338d71ae27f513b35fa257cd610e9c4e0cf
David Cerniglia <dcerniglia@thedevshop.com>
2024-01-28 09:50:21 -0500

    fixes nullable value

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -12,12 +12,12 @@ export default function Card({
   return (
     <div className="m-3 p-6 space-y-4 w-2/3 max-w-5xl min-w-60 mx-auto bg-blue-400 opacity-90 border-2 border-black">
       <div className="flex justify-between items-center">
-      <h2>{title}</h2>
+        <h2>{title}</h2>
       </div>
       <p className="text-black">{description}</p>
-      {submittedBy && (
+      {submittedBy ? (
         <p className="text-black">{`submitted by: ${submittedBy}`}</p>
-      )}
+      ) : null}
     </div>
   );
 }
